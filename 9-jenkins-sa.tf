d8a6843846c2e166051cdbf52ffc17f534a69cf2
sicher <choshitautetsu@gmail.com>
2025-08-06 22:57:28 +0800

    changed a little

--- 9-jenkins-sa.tf
+++ 9-jenkins-sa.tf
@@ -1,24 +1,24 @@
 # 1. 创建一个专用 ServiceAccount
-# resource "google_service_account" "jenkins_gke_sa" {
-#   account_id   = "jenkins-gke-sa"
-#   display_name = "Jenkins GKE CI/CD Service Account"
-# }
+resource "google_service_account" "jenkins_gke_sa" {
+  account_id   = "jenkins-gke-sa"
+  display_name = "Jenkins GKE CI/CD Service Account"
+}
 
-# # 2. 授予控制 GKE 集群的 IAM 权限（例如 Developer，Viewer 可只查阅）
-# resource "google_project_iam_member" "jenkins_gke_developer" {
-#   project = local.project_id
-#   role    = "roles/container.developer"
-#   member  = "serviceAccount:${google_service_account.jenkins_gke_sa.email}"
-# }
+# 2. 授予控制 GKE 集群的 IAM 权限（例如 Developer，Viewer 可只查阅）
+resource "google_project_iam_member" "jenkins_gke_developer" {
+  project = local.project_id
+  role    = "roles/container.developer"
+  member  = "serviceAccount:${google_service_account.jenkins_gke_sa.email}"
+}
 
-# resource "google_service_account_key" "jenkins_gke_sa" {
-#   service_account_id = google_service_account.jenkins_gke_sa.name
-#   keepers = {
-#     # optional: force recreation by changing this value
-#     expire_time = "2025-12-31T23:59:59Z"
-#   }
-#   private_key_type = "TYPE_GOOGLE_CREDENTIALS_FILE"
-# }
+resource "google_service_account_key" "jenkins_gke_sa" {
+  service_account_id = google_service_account.jenkins_gke_sa.name
+  keepers = {
+    # optional: force recreation by changing this value
+    expire_time = "2025-12-31T23:59:59Z"
+  }
+  private_key_type = "TYPE_GOOGLE_CREDENTIALS_FILE"
+}
 
 # （可选）如需管理集群也可以赋予 "roles/container.admin"
 # resource "google_project_iam_member" "jenkins_gke_admin" {
