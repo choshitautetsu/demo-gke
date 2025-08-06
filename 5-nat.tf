d4e72b6186b4793c32b4b1a31551f2cf57f2f951
sicher <choshitautetsu@gmail.com>
2025-08-06 23:14:03 +0800

    changed a little

--- 5-nat.tf
+++ 5-nat.tf
@@ -1,3 +1,4 @@
+# Cloud NAT 分配的一个静态公网 IP
 resource "google_compute_address" "nat" {
   name         = "nat"
   address_type = "EXTERNAL"
@@ -6,12 +7,15 @@ resource "google_compute_address" "nat" {
   depends_on = [google_project_service.api]
 }
 
+# 云路由器，用来管理 NAT 配置
 resource "google_compute_router" "router" {
   name    = "router"
   region  = local.region
   network = google_compute_network.vpc.id
 }
 
+# NAT 配置本体，它绑定刚才创建的路由器，指定 NAT IP 和需要 NAT 的子网
+# Cloud NAT 是用来使 Kubernetes 集群中的 Pod（或者在私有子网中的资源）能够访问外部互联网的，特别是在私有集群或者子网没有公网 IP 的场景。Pod 的对外连接会经过这个 NAT 网关转发，实现安全的公网访问
 resource "google_compute_router_nat" "nat" {
   name   = "nat"
   region = local.region
--- 6-firewalls.tf
+++ 6-firewalls.tf
@@ -9,5 +9,3 @@ resource "google_compute_firewall" "allow_iap_ssh" {
   # 限制来源 IP 为 Google IAP 的 IP 范围
   source_ranges = ["35.235.240.0/20"]
 }
-
-
